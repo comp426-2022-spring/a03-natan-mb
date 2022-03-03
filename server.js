0092a61c526380fcaee59b82f09b764d15d1fd63
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-03-02 19:58:29 -0500

    first draft

---
+++ server.js
@@ -0,0 +1,62 @@
+
+// Require Express.js
+const express = require('express')
+const app = express()
+const args = require('minimist')(process.argv.slice(2))
+args['port']
+const call = args.call
+const port = args.port || process.env.PORT || 5000
+
+
+// Start an app server
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
+});
+
+
+// Define check endpoint
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+    res.statusCode = 200;
+
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage);
+
+    // res.status(200).send('OK')
+});
+
+app.get('/app/flip/', (req, res) => {
+    res.send(Math.floor(Math.random() * 2) < 1 ? 'heads' : 'tails');
+
+});
+
+app.get('/app/flips/:number', (req, res) => {
+    const flipResults = [];
+
+    for (var i = 0; i < req.params.number; i++) {
+        flipResults.push(Math.floor(Math.random() * 2) < 1 ? 'heads' : 'tails');
+    }
+
+    res.send(flipResults);
+});
+
+app.get('/app/flip/call/heads', (req, res) => {
+    result = Math.floor(Math.random() * 2) < 1 ? 'heads' : 'tails';
+
+    res.send('{"call":"heads","flip":"' + result + '","result":"' + (result == 'heads' ? 'win' : 'lose') + '"}');
+
+});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    result = Math.floor(Math.random() * 2) < 1 ? 'heads' : 'tails';
+
+    res.send('{"call":"heads","flip":"' + result + '","result":"' + (result == 'tails' ? 'win' : 'lose') + '"}');
+});
+
+
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404).send('404 NOT FOUND')
+});
